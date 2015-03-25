bfcdf18089c0d5a1a64515dff3fef16d7dfaa5fa
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2015-03-25 01:17:20 -0600

    use start-stop-daemon, add 'reload'

--- initradio.sh
+++ initradio.sh
@@ -1,28 +1,42 @@
+#! /bin/sh
+# /etc/init.d/radio
+#
 ### BEGIN INIT INFO
 # Provides: Radio / LCD date / time / ip address
 # Required-Start: $remote_fs $syslog
 # Required-Stop: $remote_fs $syslog
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
-# Short-Description: Controll MPD
+# Short-Description: Control MPD
 # Description: Radio / LCD date / time / ip address
 ### END INIT INFO
 
 
-#! /bin/sh
-# /etc/init.d/radio
+DAEMON=/home/pi/radio/radio.py
+PIDFILE=/var/run/radio.pid
 
+start() {
+    echo "Starting Radio"
+    start-stop-daemon --start --make-pidfile --pidfile $PIDFILE --exec $DAEMON -b
+}
+
+stop() {
+    echo "Stopping Radio"
+    start-stop-daemon --stop --pidfile $PIDFILE --retry 5
+}
 
 export HOME
 case "$1" in
     start)
-        echo "Starting Radio"
-        /home/pi/radio/radio.py  2>&1 &
+        start
     ;;
     stop)
-        echo "Stopping Radio"
-	RADIO_PID=`ps auxwww | grep radio.py | head -1 | awk '{print $2}'`
-	kill -9 $RADIO_PID
+        stop
+    ;;
+    restart|force-reload)
+        stop
+        sleep 1
+        start
     ;;
     *)
         echo "Usage: /etc/init.d/radio {start|stop}"
